1731cea6370c5f05ec02051ed434fc34c39afd76
RudyDeal <rudy.deal.rd@gmail.com>
2017-05-24 11:03:04 +0200

    finition de la redirection

--- src/controller/ControllerBean.java
+++ src/controller/ControllerBean.java
@@ -1,9 +1,12 @@
 package controller;
 
+import java.io.IOException;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 @SessionScoped
@@ -13,19 +16,21 @@ public class ControllerBean implements Serializable{
 		
 	}
 	
-	public String goTo(String page) {
+	public String goTo(String page) throws IOException {
 		
 		switch(page) {
 		   case "Shop" :
-		      return "http://www.aupanierfermier.com/";
-		case "Recipe" :
-			   return "recipe";
-		case "Emergency" :
-			   return "emergency";
-		case "Home" :
-			   return "home";
-		default : 
-			   return "home";
+			   ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+			    externalContext.redirect("http://www.aupanierfermier.com/");
+		      return "Go to http://www.aupanierfermier.com/";
+			case "Recipe" :
+				   return "recipe";
+			case "Emergency" :
+				   return "emergency";
+			case "Home" :
+				   return "home";
+			default : 
+				   return "home";
 		}
 	}
 
