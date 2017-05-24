d6022660e3c75ed09416d6b32b8fea39f1bea677
MaximeCollot <maxime.collot.69@gmail.com>
2017-05-24 08:58:42 +0200

    Debut home

--- src/controller/ControllerBean.java
+++ src/controller/ControllerBean.java
@@ -3,15 +3,18 @@ package controller;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 @ManagedBean
+@SessionScoped
 public class ControllerBean implements Serializable{
 	
 	public ControllerBean (){
 		
 	}
 	
-	public void goToHome() {
+	public void goTo(String page) {
+		
 		System.out.println("home");
 	}
 
